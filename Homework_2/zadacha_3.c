ad34585aebe2aa4f659f1d1aac645f455b794663
nikito-error <nisanx@abv.bg>
2022-08-03 23:33:35 +0300

    add

--- Homework_2/zadacha_3.c
+++ Homework_2/zadacha_3.c
@@ -3,7 +3,7 @@
 int main(){
     //in terminal open location and then ./zadacha_3<numbersz3.txt
     double arr[3],max=-DBL_MAX;
-;    for(int i=0;i<3;i++){
+        for(int i=0;i<3;i++){
         scanf("%lf",&arr[i]);
         if(max<arr[i]){
             max=arr[i];
