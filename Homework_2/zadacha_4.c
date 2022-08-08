ec8aa4ebd1de9cf1689a4b77b3058ddbe11bb413
nikito-error <nisanx@abv.bg>
2022-08-08 19:34:01 +0300

    last

--- Homework_2/zadacha_4.c
+++ Homework_2/zadacha_4.c
@@ -1,17 +1,14 @@
 #include<stdio.h>
 int main(){
     //in terminal open location and then ./zadacha_4<numbersz4.txt
-    int a,n,ans=0;
+    int a,n,ans=0,b,c;
     scanf("%d",&n);//n=2 || n=3
-    if(n==3){
-        ans=1;
-    }else{
-        ans=0;
-    }
-    for(int i=0;i<n;i++){
-        scanf("%d",&a);
-        ans=ans^a;
-
+    if(n==2){
+        scanf("%d,%d",&a,&b);
+         ans=a^b;
+    }else if(n==3){
+        scanf("%d,%d,%d",&a,&b,&c);
+      ans=a^b^c;
     }
         printf("%d\n",ans);
     }
