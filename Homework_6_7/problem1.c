00f8df5424ab2ce66465c9a09ed6cb71906dadc0
nikito-error <nisanx@abv.bg>
2022-08-30 19:25:29 +0300

    1 and 2 problem

---
+++ Homework_6_7/problem1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+unsigned char CheckBit(unsigned int uValue){
+    int br=0;
+    for(int i=0;i<15;i++){
+if(!!(uValue & (1u << (i)))){
+    br++;
+}
+}
+if(br==1){
+    return 1;
+}else {
+    return 0;
+}
+}
+int main(){
+int max=65535;
+char str[16];
+fgets(str,sizeof(str),stdin);
+unsigned long long n=strtoull(str,0,16);
+if(CheckBit(n)==1){
+    printf("0x01\n");
+}else{
+    printf("0x00\n");
+}
+    return 0;
+}
---
+++ Homework_6_7/problem2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+void Reverse(char *ptr){
+char *end=ptr;
+char tmp;
+if(ptr){
+    while(*end){
+        end++;
+    }
+    end--;
+    while(ptr<end){
+        tmp=*ptr;
+        *ptr++=*end;
+        *end--=tmp;
+
+    }
+}
+}
+int main(){
+int n=200;
+char str[n];
+printf("Enter a string:\n");
+fgets(str,n,stdin);
+Reverse(str);
+printf("After reverse %s \n",str);
+    return 0;
+}
