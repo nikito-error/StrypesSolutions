af0705e294d8b29f6eac3f422be8b93435d7bc45
nikito-error <nisanx@abv.bg>
2022-08-31 16:42:58 +0300

    3rdproblem

---
+++ Homework_6_7/problem3.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void VMirror(char *pImage){
+char *end=pImage;
+char tmp;
+if(pImage){
+    while(*end){
+        end++;
+    }
+    end--;
+    while(pImage<end){
+        tmp=*pImage;
+        *pImage++=*end;
+        *end--=tmp;
+
+    }
+}
+}
+int main()
+{
+    for(int i=0;i<8;i++){
+    char arr[10]="";
+    int hexa_number = 0;
+    unsigned num_bits = 8;
+    unsigned bitmask = 1 << (num_bits - 1);
+    printf("Initial content: ");
+    scanf("%i", &hexa_number);
+
+    unsigned k = 0;
+
+
+    for(k = 0; k != num_bits; k ++)
+    {
+        if(hexa_number & (bitmask >> k))
+        {
+            strcat(arr,"1");
+        }
+        else
+        {
+            strcat(arr,"0");
+    
+        }
+    }
+    VMirror(arr);
+    char*bin=0,*a=0;
+    bin=arr;
+    a=bin;
+    int num=0;
+    do{
+        int b=*a=='1'?1:0;
+        num=(num<<1)|b;
+        a++;
+    }while(*a);
+    if(num<10){
+        printf("After vMirror 0x0%X\n",num);
+    }else{
+   printf("After vMirror 0x%X\n",num);
+    }
+    }
+    return 0;
+}
